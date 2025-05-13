6519e9b1fe2ca20fe424021d5136ad4857acecdc
Aron Ondieki Onkware <aronondieki045@gmail.com>
2025-05-13 12:17:37 +0300

    7 passed

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    #initialization takes place here
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    # returns a function to an attribute
+    @property
+    def size(self):
+        return self._size
+    # Checking whether input fits required status
+    @size.setter
+    def size(self,value):
+        if isinstance (value, int) :
+            self._size = value
+        else:
+            print("size must be an integer")
+    # a new function that prints when it is called
+    def cobble(self):
+        print("Your shoe is as good as new!")
+        # instance attribute
+        self.condition = "New" 
+    
